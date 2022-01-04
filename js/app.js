3f0376f3c39bfdc915932a89dfbefcf0d8406136
Hrishikesh <83577198+Hrishi0304@users.noreply.github.com>
2022-01-04 20:45:25 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -32,7 +32,7 @@ function showNotes() {
             <div class="card-body">
                 <h5 class="card-title">Note ${index + 1}</h5>
                 <p class="card-text">${element}</p>
-                <button id="${index}" onclick="delNote(this.id)" class="btn btn-primary">Delete Node</button>
+                <button id="${index}" onclick="delNote(this.id)" class="btn btn-primary">Delete Note</button>
             </div>
         </div>
         `;
